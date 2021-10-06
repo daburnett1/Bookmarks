d673611388fb9d72cd4119b83cb3ca09ec2197d1
daburnett1 <72366036+daburnett1@users.noreply.github.com>
2021-10-05 21:05:54 -0500

    updates to make login work properly

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -19,6 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/',
-include('account.urls')),
+    path('account/', include('account.urls')),
 ]
